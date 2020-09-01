d2ab4f80fbd0c1808b3cfd7ae8ed18a7d50c88f8
Priya Srivastava <60316903+Priya730@users.noreply.github.com>
2020-09-01 11:14:50 +0530

    Create Java Stdin and Stdout II.java

---
+++ Java Stdin and Stdout II.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        int i = scan.nextInt();
+        double d = scan.nextDouble();
+        scan.nextLine();
+        String line = scan.nextLine(); 
+        // Write your code here.
+
+        System.out.println("String: " + line);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+}
